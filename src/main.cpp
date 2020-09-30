8be815f0858d216f6c86153659b56963c00e1205
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-09-30 14:28:19 +0300

    add temporary main for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,37 @@
 #include <time.h>
 
+#include <chrono>
 #include <iostream>
 #include <random>
 
-#include "sort.h"
+#include "recursivesequentialsort.h"
+#include "sequentialsort.h"
 
 int main(int argc, char* argv[])
 {
-    size_t size;
-    std::cout << "Please, input array size: ";
-    std::cin >> size;
+    size_t size = 10;
     int* array = new int[size];
 
     srand((unsigned) time(0));
     for(size_t i = 0; i < size; i++)
     {
-        array[i] = rand() % 10000 - 5000;
+        array[i] = rand();
     }
 
-    linearSort(array, array + size - 1);
+    
+    auto sequential_begin = std::chrono::steady_clock::now();
+    //recursiveSequentialSort(array, array + size - 1);
+    sequentialSort(array, array + size - 1);
+    auto sequential_end = std::chrono::steady_clock::now();
 
-    for (size_t i = 0; i < size; i++)
+    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(sequential_end - sequential_begin).count() << "ms"<< std::endl;
+
+    /*for (size_t i = 0; i < size; i++)
     {
         std::cout << array[i] << ' ';
-    }
+    }*/
+
+    
 
     return 0;
 }
