8e46f4d12c840709d6ff4cbf946861c4fb6b3c37
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-09-30 14:15:17 +0300

    sequential sort max_order_mask counting

--- src/sequentialsort.cpp
+++ src/sequentialsort.cpp
@@ -2,8 +2,13 @@
 
 void sequentialSort(int* first, int* last)
 {
+    //Finding maximum order of array values
+    int max_value = *std::max_element(first, last);
+    int max_order_mask = 1;
+    for (; (max_value / max_order_mask) != 0; max_order_mask *= 10);
+
     std::list<int> result_list(first, last + 1);
-    for (int order_mask = 1; order_mask < 1000000; order_mask *= 10)
+    for (int order_mask = 1; order_mask < max_order_mask; order_mask *= 10)
     {
         std::vector<std::list<int>> state_vector(10);
         for (auto value : result_list)
