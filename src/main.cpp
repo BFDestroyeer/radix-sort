59b3882495aa957a34dd08841e5fc1f22e700c88
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-09-30 15:05:13 +0300

    improved test main

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[])
 {
-    size_t size = 10;
+    size_t size = 1000000;
     int* array = new int[size];
 
     srand((unsigned) time(0));
@@ -18,13 +18,22 @@ int main(int argc, char* argv[])
         array[i] = rand();
     }
 
-    
     auto sequential_begin = std::chrono::steady_clock::now();
-    //recursiveSequentialSort(array, array + size - 1);
     sequentialSort(array, array + size - 1);
     auto sequential_end = std::chrono::steady_clock::now();
+    std::cout << "Sequential: " << std::chrono::duration_cast<std::chrono::milliseconds>(sequential_end - sequential_begin).count() << "ms"<< std::endl;
 
-    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(sequential_end - sequential_begin).count() << "ms"<< std::endl;
+    srand((unsigned) time(0));
+    for(size_t i = 0; i < size; i++)
+    {
+        array[i] = rand();
+    }
+
+    auto recursive_sequential_begin = std::chrono::steady_clock::now();
+    recursiveSequentialSort(array, array + size - 1);
+    auto recursive_sequential_end = std::chrono::steady_clock::now();
+    std::cout << "Recursive sequential: " << std::chrono::duration_cast<std::chrono::milliseconds>
+        (recursive_sequential_end - recursive_sequential_begin).count() << "ms"<< std::endl;
 
     /*for (size_t i = 0; i < size; i++)
     {
