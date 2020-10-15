c15cfc35203cab238f5599841acc5a07c0059feb
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-10-15 10:55:38 +0300

    add documentation

--- include/ompsort.h
+++ include/ompsort.h
@@ -4,4 +4,10 @@
 
 #include <omp.h>
 
+/**
+ * @brief Sort algorithm for Little endian on OpenMP
+ * 
+ * @param first array first element pointer
+ * @param last array last element pointer
+ */
 void ompSort(int* first, int* last);
